e33cd1c0932a2232c98a78b2be1d47742c3f50a9
Zzzarix <android1vvv@gmail.com>
2023-08-28 10:38:48 +1200

    Implemented CRUD base methods

--- onetotwo/manager.py
+++ onetotwo/manager.py
@@ -1,15 +1,15 @@
 # %% Import dependencies
+import uuid
 from abc import ABC
 from datetime import datetime
 from typing import Generic, TypeVar
-import uuid
+
 import firebase_admin as firebase
 from firebase_admin import db
-
 from onetotwo.model import FireBaseModel
 
-
 T = TypeVar("T", FireBaseModel)
+
 
 # %% Models
 class FireBaseManager(Generic[T], ABC):
@@ -19,15 +19,29 @@ class FireBaseManager(Generic[T], ABC):
         """Init FireBaseManager"""
         self._app: firebase.App = firebase.get_app(app_name)
         self._ref = db.reference(f"/{model_name}/", app=self._app)
-    
-    def create(self, **kwargs) -> T:
+
+    def _create(self, **kwargs) -> T:
         """Create model"""
 
         kwargs["uid"] = uuid.uuid4()
         kwargs["created_at"] = datetime.utcnow()
 
-        model: FireBaseModel = T(**kwargs)
+        model = T(**kwargs)
 
         self._ref.child(model.uid).set(model.to_json())
 
         return model
+
+    def _get(self, uid: str) -> T:
+        """Get model"""
+        res = self._ref.child(uid).get()
+
+        return T(**res)
+
+    def _update(self, uid: str, **kwargs) -> None:
+        """Update model"""
+        self._ref.child(uid).update(kwargs)
+
+    def _delete(self, uid: str) -> None:
+        """Delete model"""
+        self._ref.child(uid).delete()
