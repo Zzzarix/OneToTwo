7c8d436d1621b1031c2b01c9d7dab8d690f8d3a2
Zzzarix <android1vvv@gmail.com>
2023-08-28 22:09:56 +1200

    managers fix

--- onetotwo/user/manager.py
+++ onetotwo/user/manager.py
@@ -1,4 +1,6 @@
 # %% Import Dependencies
+from typing import Type
+
 from onetotwo.manager import FireBaseManager
 from onetotwo.user.model import User, UserLocale
 
@@ -7,8 +9,8 @@ from onetotwo.user.model import User, UserLocale
 class UserManager(FireBaseManager[User]):
     """User firebase manager"""
 
-    def __init__(self, app_name: str, model_name: str) -> None:
-        super().__init__(app_name, model_name)
+    def __init__(self, app_name: str, model: Type[User]) -> None:
+        super().__init__(app_name, model)
 
     def create(self, name: str, email: str, password: str, locale: UserLocale) -> User:
         """Create User model"""
