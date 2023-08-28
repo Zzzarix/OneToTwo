d9b88a8114eb97d56d580b40ce6bcb076d6a197b
Zzzarix <android1vvv@gmail.com>
2023-08-28 19:09:44 +1200

    OneWay models

---
+++ onetotwo/oneway/model.py
@@ -0,0 +1,48 @@
+# %% Import dependencies
+from datetime import datetime
+from enum import Enum
+from typing import Dict, Optional
+
+from onetotwo.model import ImmutableModel, MutableModel
+
+
+# %% Enums
+class WayLifetime(Enum, int):
+    Permanent = 0
+    Day = 1
+    ThreeDays = 3
+    Week = 7
+    Month = 30
+
+
+# %% Models
+class TargetUrl(MutableModel):
+    """Target url model"""
+
+    is_secured: bool
+    domain: str
+    path: str
+    params: Dict[str, str]
+
+    def format(self) -> str:
+        return self.domain + self.path + "?" + "&".join([k + "=" + v for k, v in self.params.items()])
+
+
+class OneWay(MutableModel):
+    """Shortened link model"""
+
+    name: str
+    target: TargetUrl
+    alias: str
+    is_temporary: bool
+    lifetime: WayLifetime
+    user_uid: Optional[str]
+    created_at: datetime
+
+
+class Redirect(ImmutableModel):
+    """Redirect model"""
+
+    ip: str
+    oneway_uid: str
+    created_at: datetime
