c166211ef8364c702a4d340dc9ab6a902f3a39ad
Zzzarix <android1vvv@gmail.com>
2023-08-28 19:11:30 +1200

    User manager impl

---
+++ onetotwo/user/manager.py
@@ -0,0 +1,23 @@
+# %% Import Dependencies
+from onetotwo.manager import FireBaseManager
+from onetotwo.user.model import User, UserLocale
+
+
+# %% Manager
+class UserManager(FireBaseManager[User]):
+    """User firebase manager"""
+
+    def __init__(self, app_name: str, model_name: str) -> None:
+        super().__init__(app_name, model_name)
+
+    def create(self, name: str, email: str, password: str, locale: UserLocale) -> User:
+        """Create User model"""
+        return self._create(name=name, email=email, password=password, locale=locale, is_active=True)
+
+    def get(self, uid: str) -> User:
+        """Get user model"""
+        return self._get(uid)
+
+    def delete(self, uid: str) -> None:
+        """Delete user model"""
+        self._delete(uid)
